797405d032ca359229328b67c01821243327573a
ronaks9065 <115705180+ronaks9065@users.noreply.github.com>
2022-12-01 09:57:03 +0530

    Update buildspec_yml.sh

--- scripts/buildspec_yml.sh
+++ scripts/buildspec_yml.sh
@@ -1,13 +1,9 @@
 #!/bin/bash
 
-# install and use node version
-nvm install 14.4.0
-nvm use 14.4.0
-
-# print node and npm version
-node --version
-npm --version
-
 # upgrade npm and install forever
 npm install -g npm@latest
 npm install -g forever
+
+
+node --version
+npm --version
