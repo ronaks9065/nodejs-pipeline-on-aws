b48332b43019337bacde5f02466ee5e3f34158af
ronaks9065 <115705180+ronaks9065@users.noreply.github.com>
2023-03-19 09:34:07 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,10 +1,31 @@
-const express = require('express');
-const port = 2021;
+const AWS = require('aws-sdk');
+const fs = require('fs');
+const path = require('path');
 
-const app = express();
-app.get('/', (req, res) => {
-    res.send('A Simple Node.js Server is Up and Running.......!')
-})
-app.listen(port, ()=>{
-    console.log(`Server is listening on port ${port}`)
+//configuring the AWS environment
+AWS.config.update({
+    accessKeyId: "AKIAXDKTCKZ4OLAXLBHQ",
+    secretAccessKey: "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"
+  });
+
+var s3 = new AWS.S3();
+var filePath = "./data/file.txt";
+
+//configuring parameters
+var params = {
+  Bucket: 'www.ronakcodep.tk',
+  Body : fs.createReadStream(filePath),
+  Key : "folder/"+Date.now()+"_"+path.basename(filePath)
+};
+
+s3.upload(params, function (err, data) {
+  //handle error
+  if (err) {
+    console.log("Error", err);
+  }
+
+  //success
+  if (data) {
+    console.log("Uploaded in:", data.Location);
+  }
 });
